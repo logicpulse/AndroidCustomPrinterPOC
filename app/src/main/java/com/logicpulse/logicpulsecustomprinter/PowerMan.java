553b6f57a1247ffe72e8e1d6534251c027ab89e7
Mário Monteiro <mario.monteiro@logicpulse.com>
2016-10-07 16:46:17 +0100

    Inc minSdkVersion 15 to minSdkVersion 20

--- app/src/main/java/com/logicpulse/logicpulsecustomprinter/PowerMan.java
+++ app/src/main/java/com/logicpulse/logicpulsecustomprinter/PowerMan.java
@@ -39,7 +39,7 @@ public class PowerMan {
             return mScreenOn;
         }
 
-        mScreenOn = mPowerManager.isScreenOn();
+        mScreenOn = mPowerManager.isInteractive();
 
         Log.d(TAG, "==getScreenOn[" + mScreenOn + "]");
 
